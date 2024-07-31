e14c76f71849d98c8504e4c62c3dff0e479b5cc2
Clint Villaflores <118436424+Ryatsu@users.noreply.github.com>
2024-07-31 11:07:52 +0800

    test
    users => user

--- Feedback/src/main/java/org/example/admin/model/UserModel.java
+++ Feedback/src/main/java/org/example/admin/model/UserModel.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
